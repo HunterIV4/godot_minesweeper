d2e17ca2674277d4f3d254c27ae3df0a883add77
HunterIV4 <hunteriv4@gmail.com>
2025-01-10 15:04:02 -0800

    Refactored tile textures into a custom resource and set the board as the source of this resource. Set up basic board generation and enabled tile interaction code.

--- scenes/Board/board.gd
+++ scenes/Board/board.gd
@@ -1,1 +1,20 @@
+class_name Board
 extends GridContainer
+
+@export var rows: int
+@export var cols: int
+@export var tile_textures: TileTextures
+
+func _ready() -> void:
+	columns = cols
+	generate_board_base()
+
+func generate_board_base() -> void:
+	var total_tiles = rows * cols
+	for _t in total_tiles:
+		add_tile()
+
+func add_tile():
+	var new_tile = Tile.new()
+	new_tile.set_textures(tile_textures)
+	add_child(new_tile)
--- scenes/Tile/tile.gd
+++ scenes/Tile/tile.gd
@@ -1,21 +1,8 @@
-extends TextureButton
 class_name Tile
+extends TextureButton
 
 enum State {SAFE, CAUTION, MINE}
-
-@export var img_hidden: Texture2D
-@export var img_safe: Texture2D
-@export var img_mine: Texture2D
-@export var img_mine_selected: Texture2D
-@export var img_flagged: Texture2D
-@export var img_1: Texture2D
-@export var img_2: Texture2D
-@export var img_3: Texture2D
-@export var img_4: Texture2D
-@export var img_5: Texture2D
-@export var img_6: Texture2D
-@export var img_7: Texture2D
-@export var img_8: Texture2D
+@export var textures: TileTextures
 
 var mines_nearby: int = 0
 var state: State = State.SAFE
@@ -31,32 +18,40 @@ func reveal() -> void:
 	is_hidden = false
 
 
+func set_textures(tile_textures: TileTextures):
+	textures = tile_textures
+	_update_state()	
+
+
 func _on_update_tile_state(tile: Tile, new_state: State) -> void:
 	if tile != self: return
 	state = new_state
 	
-	if is_hidden: return	# Don't update visuals if still hidden
 	_update_state()
-	
+
+
 func _update_state() -> void:
-	match state:
-		State.SAFE:
-			texture_normal = img_safe
-		State.MINE:
-			if is_flagged:
-				texture_normal = img_mine_selected
-			else:
-				texture_normal = img_mine
-		State.CAUTION:
-			_set_caution_state()
+	if is_hidden:
+		_set_flag_state()
+	else:
+		match state:
+			State.SAFE:
+				texture_normal = textures.safe
+			State.MINE:
+				if is_flagged:
+					texture_normal = textures.mine_selected
+				else:
+					texture_normal = textures.mine
+			State.CAUTION:
+				_set_caution_state()
 
 
 func _set_flag_state() -> void:
 	if not is_hidden: return
 	if is_flagged:
-		texture_normal = img_flagged
+		texture_normal = textures.flagged
 	else:
-		texture_normal = img_hidden
+		texture_normal = textures.hidden
 
 
 func _set_caution_state() -> void:
@@ -64,22 +59,23 @@ func _set_caution_state() -> void:
 	assert(mines_nearby > 0 and mines_nearby < 9, "_set_caution_state() called with an invalid number of nearby mines (1-8 are valid): " + str(mines_nearby))
 	
 	match mines_nearby:
-		1: texture_normal = img_1
-		2: texture_normal = img_2
-		3: texture_normal = img_3
-		4: texture_normal = img_4
-		5: texture_normal = img_5
-		6: texture_normal = img_6
-		7: texture_normal = img_7
-		8: texture_normal = img_8
+		1: texture_normal = textures.c_1
+		2: texture_normal = textures.c_2
+		3: texture_normal = textures.c_3
+		4: texture_normal = textures.c_4
+		5: texture_normal = textures.c_5
+		6: texture_normal = textures.c_6
+		7: texture_normal = textures.c_7
+		8: texture_normal = textures.c_8
 
-func _on_gui_input(event: InputEvent) -> void:
+
+func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and is_hidden:
 		match event.button_index:
 			MOUSE_BUTTON_LEFT:
 				Events.tile_pressed.emit(self, MouseButton.MOUSE_BUTTON_LEFT)
 				
-				is_hidden = false
+				reveal()
 				_update_state()
 				
 			MOUSE_BUTTON_RIGHT:
